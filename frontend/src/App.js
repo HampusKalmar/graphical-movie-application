a59f9258e57b2b9434f46949178ebea26ad866da
Hampus Tuisku <ht222mi@student.lnu.se>
2024-04-22 12:33:44 +0200

    Add barchart to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,13 @@
 import './App.css'
-import { useState, useEffect } from 'react';
+import BarChart from './components/BarChart';
 
 
 function App() {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    fetch('/movie-data')
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      })
-      .catch((error) => console.log(error))
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my App using React and Flask</h1>
-        {data ? (
-          <ul>
-            {data.map((item, index) => (
-              <li key={index}>
-                {'IMDb Rating: ' + item['IMDB Rating']} {'Name of Movie: ' 
-                + item['Movie Name']} {'Movie Rank: ' 
-                + item['Movie Rank']} {'Year of release: ' 
-                + item['Year of Release']}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>Loading data...</p>
-        )}
+        <h1>Movie Data</h1>
+        <BarChart />
       </header>
     </div>
   );
---
+++ frontend/src/components/BarChart.js
@@ -0,0 +1,64 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js'
+import { Bar } from 'react-chartjs-2';
+import { useState, useEffect } from 'react';
+ChartJS.register(
+  CategoryScale, 
+  LinearScale,
+  BarElement,
+  Tooltip, 
+  Legend,
+  Title
+);
+
+function BarChart() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('/movie-data')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  const chartData = {
+    labels: data.map(item => item['Movie Name']),
+    datasets: [
+      {
+        label: 'IMDB Rating',
+        data: data.map(item => item['IMDB Rating']),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'IMDB Ratings by Movie'
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+ 
+  return (
+    <div class='size-full...'>
+      <h2>Bar Chart</h2>
+      {data.length ? <Bar data={chartData} options={options}/> : <p>No data to display</p>}
+    </div>
+  );
+}
+
+export default BarChart;
